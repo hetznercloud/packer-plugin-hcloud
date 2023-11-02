5daf35b50a6572bb78328a01d83b877aa657ec0c
pauhull <pauhull3@gmail.com>
2023-11-02 13:54:24 +0100

    ci: add release please

--- version/version.go
+++ version/version.go
@@ -7,11 +7,13 @@ import "github.com/hashicorp/packer-plugin-sdk/version"
 
 var (
 	// Version is the main version number that is being run at the moment.
-	Version = "1.1.2"
+	Version = "1.1.2" // x-release-please-version
 
 	// VersionPrerelease is A pre-release marker for the Version. If this is ""
 	// (empty string) then it means that it is a final release. Otherwise, this
 	// is a pre-release such as "dev" (in development), "beta", "rc1", etc.
+	//
+	// For releases, GoReleaser will automatically set this to an empty string.
 	VersionPrerelease = "dev"
 
 	// PluginVersion is used by the plugin set to allow Packer to recognize
