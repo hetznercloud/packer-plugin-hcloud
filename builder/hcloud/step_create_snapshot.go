3e3265896996a9a23dad1b74a6ba2beeb5cd2507
jo <ljonas@riseup.net>
2024-04-30 15:36:03 +0200

    refactor: use new client.Action.WaitFor

--- builder/hcloud/step_create_snapshot.go
+++ builder/hcloud/step_create_snapshot.go
@@ -33,9 +33,8 @@ func (s *stepCreateSnapshot) Run(ctx context.Context, state multistep.StateBag) 
 	state.Put(StateSnapshotID, result.Image.ID)
 	state.Put(StateSnapshotName, c.SnapshotName)
 
-	_, errCh := client.Action.WatchProgress(ctx, result.Action)
-	if err1 := <-errCh; err1 != nil {
-		return errorHandler(state, ui, "Could not create snapshot", err1)
+	if err := client.Action.WaitFor(ctx, result.Action); err != nil {
+		return errorHandler(state, ui, "Could not create snapshot", err)
 	}
 
 	oldSnap, found := state.GetOk(StateSnapshotIDOld)
