c779c5b2ee25aa7b80edffcbbc1262d677f7fa5b
Julian Tölle <julian.toelle@hetzner-cloud.de>
2025-03-18 11:48:18 +0100

    ci: switch to releaser-pleaser
    Switch to [releaser-pleaser](https://github.com/apricote/releaser-pleaser) for planning new releases. This adds the following new features:
    
    - We can easily cut pre-releases, these are always useful if we want to allow some customers to test features before making an official release or to validate changes made to the release CI workflows.
    - We can add our own release notes to each release to better inform customers about the new features we added or about any deprecations.

--- version/version.go
+++ version/version.go
@@ -7,7 +7,7 @@ import "github.com/hashicorp/packer-plugin-sdk/version"
 
 var (
 	// Version is the main version number that is being run at the moment.
-	Version = "1.6.0" // x-release-please-version
+	Version = "1.6.0" // x-releaser-pleaser-version
 
 	// VersionPrerelease is A pre-release marker for the Version. If this is ""
 	// (empty string) then it means that it is a final release. Otherwise, this
